ea1d26654805f8d4fc064504959ea7af30323a02
Ayush Rudani <ayushcrudani@gmail.com>
2023-11-16 09:33:58 +0530

    AcCreation component Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import NavBar from "@/components/NavBar";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import AcCreation from "./AcCreation";
 
 
 function Home() {
     const router = useRouter();
     return (
-        <NavBar />
+        <>
+            <NavBar />
+            <AcCreation />
+        </>
     );
 }
 
