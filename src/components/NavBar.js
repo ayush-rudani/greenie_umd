f9fef315e47c0cea1e57427c84700a08c6c7caa2
Ayush Rudani <ayushcrudani@gmail.com>
2023-11-16 23:39:19 +0530

    edited properties

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,45 +6,45 @@ import { useRouter } from "next/router";
 export default function () {
     return (
         <nav
-            class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-green-400 backdrop-filter backdrop-blur-xl rounded-lg m-2">
-            <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
-                <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
-                    <span class="font-semibold text-xl tracking-tight">Greenie UMD</span>
+            className="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-green-400 backdrop-filter backdrop-blur-xl rounded-lg m-2">
+            <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+                <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
+                    <span className="font-semibold text-xl tracking-tight">Greenie UMD</span>
                 </div>
-                <div class="block lg:hidden ">
+                <div className="block lg:hidden ">
                     <button
                         id="nav"
-                        class="flex items-center px-3 py-2 border-2 rounded border-green-400  text-green-400  hover:text-green-400 hover:border-green-400 ">
+                        className="flex items-center px-3 py-2 border-2 rounded border-green-400  text-green-400  hover:text-green-400 hover:border-green-400 ">
                     </button>
                 </div>
             </div>
 
-            <div class="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
-                <div class="text-md font-bold text-slate-700 lg:flex-grow">
+            <div className="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
+                <div className="text-md font-bold text-slate-700 lg:flex-grow">
                     <Link href="#responsive-header"
-                        class="block group mt-4 lg:inline-block lg:mt-0 hover:text-green-400 px-4 py-2 rounded  mr-2">
+                        className="block group mt-4 lg:inline-block lg:mt-0 hover:text-green-400 px-4 py-2 rounded  mr-2">
                         User Details
-                        <div class="bg-green-400 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                        <div className="bg-green-400 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
                     </Link>
 
                 </div>
 
-                <div class="relative mx-auto text-gray-600 lg:block hidden">
+                <div className="relative mx-auto text-gray-600 lg:block hidden">
                     <input
-                        class="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
+                        className="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
                         type="search" name="search" placeholder="Search" />
-                    <button type="submit" class="absolute right-0 top-0 mt-3 mr-2">                    </button>
+                    <button type="submit" className="absolute right-0 top-0 mt-3 mr-2">                    </button>
                 </div>
-                <div class="flex ">
+                <div className="flex ">
                     <Link href="#"
-                        class="block group text-md px-4 py-2 rounded text-slate-700 ml-2 font-bold hover:text-green-400 mt-4 lg:mt-0">Sign
+                        className="block group text-md px-4 py-2 rounded text-slate-700 ml-2 font-bold hover:text-green-400 mt-4 lg:mt-0">Sign
                         in
-                        <div class="bg-green-400 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                        <div className="bg-green-400 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
                     </Link>
 
                     <Link href="#"
-                        class="block group text-md px-4 ml-2 py-2 rounded text-slate-700 font-bold hover:text-green-400 mt-4 lg:mt-0">login
-                        <div class="bg-green-400 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
+                        className="block group text-md px-4 ml-2 py-2 rounded text-slate-700 font-bold hover:text-green-400 mt-4 lg:mt-0">login
+                        <div className="bg-green-400 h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
                     </Link>
 
                 </div>
--- src/pages/UserDetail.js
+++ src/pages/UserDetail.js
@@ -9,6 +9,9 @@ export default function UserDetail() {
     const [users, setUsers] = useState([]);
     const [user, setUser] = useState('');
 
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const [search, setSearch] = useState('');
 
     const fetchUsers = async () => {
@@ -32,28 +35,34 @@ export default function UserDetail() {
         item.username.toLowerCase().includes(search.toLowerCase())
     );
 
+    function handleUserSelection(user) {
+        setSelectedUser(user);
+        setIsModalOpen(true);
+        console.log(user);
+    }
+
     return (
         <>
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg m-5 w-5/6 mx-auto">
-                <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white  m-2">
-                    <label for="table-search" class="sr-only">Search</label>
-                    <div class="relative">
-                        <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500" aria-hidden="true" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg m-5 w-5/6 mx-auto">
+                <div className="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white  m-2">
+                    <label htmlFor="table-search" className="sr-only">Search</label>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="text" id="s_users" value={search} onChange={(e) => setSearch(e.target.value)} class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-green-500 focus:border-green-500" placeholder="Search for users" />
+                        <input type="text" id="s_users" value={search} onChange={(e) => setSearch(e.target.value)} className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-green-500 focus:border-green-500" placeholder="Search for users" />
                     </div>
                 </div>
-                <table class="w-full text-sm text-left rtl:text-right text-gray-50">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-50">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                         <tr>
-                            <th scope="col" class="px-6 py-3">ID</th>
-                            <th scope="col" class="px-6 py-3">Name</th>
-                            <th scope="col" class="px-6 py-3">Phone</th>
-                            <th scope="col" class="px-6 py-3">Creation date</th>
-                            <th scope="col" class="px-6 py-3">Action</th>
+                            <th scope="col" className="px-6 py-3">ID</th>
+                            <th scope="col" className="px-6 py-3">Name</th>
+                            <th scope="col" className="px-6 py-3">Phone</th>
+                            <th scope="col" className="px-6 py-3">Creation date</th>
+                            <th scope="col" className="px-6 py-3">Action</th>
                         </tr>
                     </thead>
 
@@ -61,23 +70,23 @@ export default function UserDetail() {
                         {
                             filteredUser.map(u => {
                                 return (
-                                    <tr class="bg-white border-b hover:bg-gray-50">
-                                        <td class="px-7 py-4 text-gray-500">
+                                    <tr className="bg-white border-b hover:bg-gray-50" key={u.id}>
+                                        <td className="px-7 py-4 text-gray-500">
                                             {u.id}
                                         </td>
-                                        <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
-                                            <img class="w-10 h-10 rounded-full" src="user.png" alt="" />
-                                            <div class="ps-3">
-                                                <div class="text-base font-semibold">{u.username}</div>
-                                                <div class="font-normal text-gray-500">{u.email}</div>
+                                        <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
+                                            <img className="w-10 h-10 rounded-full" src="user.png" alt="" />
+                                            <div className="ps-3">
+                                                <div className="text-base font-semibold">{u.username}</div>
+                                                <div className="font-normal text-gray-500">{u.email}</div>
                                             </div>
                                         </th>
-                                        <td class="px-6 py-4 text-gray-950">{u.phone}</td>
-                                        <td class="px-6 py-4">
-                                            <div class="flex items-center text-gray-800">{u.creationDate}</div>
+                                        <td className="px-6 py-4 text-gray-950">{u.phone}</td>
+                                        <td className="px-6 py-4">
+                                            <div className="flex items-center text-gray-800">{u.creationDate}</div>
                                         </td>
-                                        <td class="px-6 py-4">
-                                            <a href="#" class="font-medium text-green-600  hover:underline">Edit user</a>
+                                        <td className="px-6 py-4">
+                                            <div href="#" className="font-medium text-green-600  hover:underline hover:cursor-pointer" onClick={() => handleUserSelection(u)}>Edit user</div>
                                         </td>
                                     </tr>
                                 )
